48b12118c276a94f45079327a36ea0eb1dd8842b
rafael <rafaelsistem87@gmail.com>
2021-10-30 07:47:08 -0700

    edit button

--- js/script2.js
+++ js/script2.js
@@ -3,6 +3,8 @@ const todoInput = document.querySelector('.todo-input');
 const todoItemsList = document.querySelector('.todo-items');
 
 let todos = [];
+let edit = false
+let editID 
 
 todoForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -10,36 +12,49 @@ todoForm.addEventListener('submit', function(event) {
 });
 
 function addTodo(item) {
-  if (item !== '') {
-    const todo = {
-      id: Date.now(),
-      name: item,
-      completed: false
-    };
+  console.log(todos)
 
-    todos.push(todo);
-    addToLocalStorage(todos)
-    todoInput.value = '';
+  if(edit){
+    todos.forEach(el => el.id == editID ? el.name = todoInput.value : el) 
+    addToLocalStorage(todos) 
+    edit = false
+  }else{
+
+    if (item !== '') {
+      const todo = {
+        id: Date.now(),
+        name: item,
+        completed: false
+      };
+
+      todos.push(todo);
+      addToLocalStorage(todos)
+    }
+
   }
+  todoInput.value = '';
 }
 
 function renderTodos(todos) {
 
   todoItemsList.innerHTML = '';
+
   todos.forEach(function(item) {
     const checked = item.completed ? 'checked': null;
     const li = document.createElement('li');
     li.setAttribute('class', 'item');
-    li.setAttribute('data-key', item.id);
+    li.setAttribute('id', item.id);
  
-    if (item.completed === true) {
-      li.classList.add('checked');
-    }
+    item.completed ? li.classList.add('checked') : null
+    
 
     li.innerHTML = `
       <input type="checkbox" class="checkbox" ${checked}>
+
       ${item.name}
+
       <button class="delete-button">X</button>
+      <button class="edit-button">Edit</button>
     `;
 
     todoItemsList.append(li);
@@ -61,28 +76,40 @@ function getFromLocalStorage() {
 }
 
 function toggle(id) {
-  todos.forEach(function(item) {
-    if (item.id == id) {
-      item.completed = !item.completed;
-    }
-  });
-  addToLocalStorage(todos);
+  todos.forEach(item => item.id == id ? item.completed = !item.completed : item)
+  addToLocalStorage(todos)
+  console.log(todos)
 }
 
 function deleteTodo(id) {
-  todos = todos.filter(function(item) {
-    return item.id != id;
-  });
+  todos = todos.filter(item => item.id != id)
   addToLocalStorage(todos);
+}
+
+function editTodo(id){
+  const editName = todos.find(el => el.id == id ).name
+  todoInput.value = editName
+
+  edit = true
+
+  sendEditId(id)
+}
+
+function sendEditId(id){
+  editID = id
 }
 
 getFromLocalStorage();
 
 todoItemsList.addEventListener('click', function(event) {
   if (event.target.type === 'checkbox') {
-     toggle(event.target.parentElement.getAttribute('data-key'));
+     toggle(event.target.parentElement.getAttribute('id'));
   }
   if (event.target.classList.contains('delete-button')) {
-     deleteTodo(event.target.parentElement.getAttribute('data-key'));
+     deleteTodo(event.target.parentElement.getAttribute('id'));
   }
+
+  if (event.target.classList.contains('edit-button')) {
+    editTodo(event.target.parentElement.getAttribute('id'));
+ }
 });
